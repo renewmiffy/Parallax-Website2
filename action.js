15d422ebb19b18bada1a3e18c3b5197588e411b0
renewmiffy <renewmiffy@gmail.com>
2025-12-06 18:23:42 +0800

    Change fish swim direction vertically

--- action.js
+++ action.js
@@ -1,10 +1,16 @@
 // Select the class bubble
 time = document.getElementsByClassName('bubbles')[0];
 
-// padding values for desktop
-var fish2move = 100;
-var fish3move = 900;
-var fish4move = 1200;
+// Cache fish elements and their starting positions for vertical movement
+const fishes = [
+    { element: fish1, speed: 0.25 },
+    { element: fish2, speed: 0.22 },
+    { element: fish3, speed: 0.18 },
+    { element: fish4, speed: 0.14 }
+].map(fish => ({
+    ...fish,
+    baseTop: parseFloat(getComputedStyle(fish.element).top)
+}));
 
 if (screen.width < 400) {
 
@@ -12,10 +18,11 @@ if (screen.width < 400) {
     time.style.setProperty('--transform-duration', '15s')
     time.style.setProperty('--transform-y', '-700vh')
 
-    // padding values for mobile
-    fish2move = 1680;
-    fish3move = 3000;
-    fish4move = 4300;
+    // Slightly slow down upward swim on smaller screens for readability
+    fishes[0].speed = 0.2;
+    fishes[1].speed = 0.17;
+    fishes[2].speed = 0.14;
+    fishes[3].speed = 0.1;
 }
 
 
@@ -49,11 +56,10 @@ window.addEventListener('scroll', function () {
         splash.style.top = 20 + value * -0.3 + 'px';
     }
 
-    //Move fishes horizontally
-    fish1.style.right = (value - 100) * 1 + 'px';
-    fish2.style.left = (value - fish2move) * 1 + 'px';
-    fish3.style.right = (value - fish3move) * 1 + 'px';
-    fish4.style.left = (value - fish4move) * 1 + 'px';
+    // Move fishes vertically from bottom to top
+    fishes.forEach(fish => {
+        fish.element.style.top = fish.baseTop - value * fish.speed + 'px';
+    });
 })
 
 
